4d9b89e9a0c309e3b50700bb392d05391003746c
jshwi <stephen@jshwisolutions.com>
2023-06-20 18:12:24 +1000

    add: add parser
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
+}
+
+/// Dotfile manager.
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Add new FILE to version control.
+    Add {},
+    /// Clone a dotfile repository from a URL.
+    Clone {},
+    /// Commit changes to FILE.
+    Commit {},
+    /// Install a dotfile repository.
+    Install {},
+    /// Create a new LINK from TARGET.
+    Link {},
+    /// List all versioned dotfiles.
+    List {},
+    /// Push changes to remote.
+    Push {},
+    /// Remove FILE from version control.
+    Remove {},
+    /// Check version status.
+    Status {},
+    /// Revert previous commit and actions.
+    Undo {},
+    /// Uninstall a dotfile repository.
+    Uninstall {},
 }

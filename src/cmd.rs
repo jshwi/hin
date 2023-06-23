b60020406124bfd250912544720b4f3610dc4ec9
jshwi <stephen@jshwisolutions.com>
2023-06-24 09:36:23 +1000

    add: add add
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- src/cmd.rs
+++ src/cmd.rs
@@ -3,7 +3,7 @@ use std::{
     fs::{create_dir, remove_file, rename},
     io::ErrorKind,
     os::unix::fs::symlink,
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use color_eyre::Result;
@@ -57,7 +57,66 @@ pub fn list() -> Result<()> {
 
 
 pub fn add(file: String) -> Result<()> {
-    todo!("add {}", file);
+    let mut entry = PathBuf::from(&file);
+    let dotfiles = &env::var(DOTFILES)?;
+    let dotfiles = Path::new(dotfiles);
+    let path = Path::new(dotfiles).join("dotfiles.ini");
+    debug!("listing dotfiles configured in {:?}", path);
+    let config = Ini::load_from_file(path).unwrap();
+    for (_, prop) in &config {
+        if prop.contains_key(&file) {
+            // todo
+            //   make this an error
+            panic!("{} already added", &file)
+        }
+    }
+    if entry.is_symlink() {
+        entry = entry.read_link()?;
+        if !entry.exists() {
+            // todo
+            //   make this an error
+            panic!("{} is a dangling symlink", &file)
+        }
+    }
+    if entry.exists() {
+        entry = entry.parent().unwrap().join(format!(
+            "{:?}.{:?}",
+            entry.file_name(),
+            chrono::Utc::now().timestamp()
+        ));
+    }
+    match rename(&entry, dotfiles.join(entry.file_name().unwrap())) {
+        Ok(_) => {
+            if entry.is_dir() {
+                // add .gitignore files and check for any children that
+                // might already be versioned
+                add_dir(&entry)
+            }
+            // todo
+            //   config.add(entry)
+        }
+        Err(_) => {
+            if !add_child(&entry) {
+                // todo
+                //   make this an error
+                panic!("{} not found", &file)
+            }
+        }
+    }
+    println!("added {:?}", entry);
+    // todo
+    //  commit here
+    Ok(())
+}
+
+
+fn add_dir(p0: &Path) {
+    todo!("add dir {:?}", p0)
+}
+
+
+fn add_child(p0: &Path) -> bool {
+    todo!("add child {:?}", p0)
 }
 
 

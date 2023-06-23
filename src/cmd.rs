cf780ed91dcf0c7f3d723fa9d4936f7ef232272c
jshwi <stephen@jshwisolutions.com>
2023-06-23 19:33:01 +1000

    add: add uninstall
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- src/cmd.rs
+++ src/cmd.rs
@@ -13,7 +13,6 @@ use log::debug;
 
 use crate::DOTFILES;
 
-
 pub fn clone(url: String) -> Result<()> {
     let dotfiles = &env::var(DOTFILES)?;
     let path = Path::new(&dotfiles);
@@ -181,6 +180,20 @@ pub fn undo() -> Result<()> {
 
 
 pub fn uninstall() -> Result<()> {
-    debug!("uninstalled");
+    let dotfiles = &env::var(DOTFILES)?;
+    let path = Path::new(dotfiles).join("dotfiles.ini");
+    debug!("installing dotfiles configured in {:?}", path);
+    let config = Ini::load_from_file(path).unwrap();
+    println!("removed the following dotfiles:");
+    for (_, prop) in &config {
+        for (key, _) in prop.iter() {
+            let key_path = shellexpand::env(&key)?.to_string();
+            let key_path = Path::new(&key_path);
+            if key_path.is_symlink() {
+                println!("\t{:?}", key_path);
+                remove_file(key_path)?;
+            }
+        }
+    }
     Ok(())
 }

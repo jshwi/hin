6c4b457453367754695e88de166e8186705cdab1
jshwi <stephen@jshwisolutions.com>
2023-06-24 10:09:15 +1000

    fix: fix user data dir
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- src/misc.rs
+++ src/misc.rs
@@ -9,7 +9,10 @@ pub fn set_repo_path() -> Result<()> {
     let name: String = env::var("CARGO_PKG_NAME")?;
     debug!("CARGO_PKG_NAME={}", name);
     let repo = env::var(DOTFILES);
-    let default = directories::BaseDirs::new().unwrap().data_dir().join(name);
+    let base_dirs = directories::BaseDirs::new().unwrap();
+    let user_data_dir = base_dirs.data_dir();
+    env::set_var("USER_DATA_DIR", user_data_dir);
+    let default = user_data_dir.join(name);
     if repo.is_err() {
         debug!("DOTFILES not set, defaulting to {:?}", default);
         env::set_var(DOTFILES, default)

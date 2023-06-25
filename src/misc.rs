61be05b38d2b49b562c7a818c469e61bec8696cc
jshwi <stephen@jshwisolutions.com>
2023-06-25 19:31:27 +1000

    refactor: remove unused funcs
    Signed-off-by: jshwi <stephen@jshwisolutions.com>

--- src/misc.rs
+++ src/misc.rs
@@ -1,14 +1,9 @@
-use std::{
-    env,
-    fs,
-    path::{Path, PathBuf},
-};
+use std::{env, fs, path::Path};
 
 use color_eyre::Result;
 use log::debug;
 
 use crate::DOTFILES;
-
 
 pub fn set_repo_path() -> Result<()> {
     let name: String = env::var("CARGO_PKG_NAME")?;
@@ -30,27 +25,4 @@ pub fn set_repo_path() -> Result<()> {
         debug!("creating {}", dotfiles);
     }
     Ok(())
-}
-
-
-pub fn is_child_of(this: &str, other_key: &Path, other_value: &Path) -> bool {
-    this.starts_with(other_key.to_str().unwrap())
-        || this.starts_with(other_value.to_str().unwrap())
-}
-
-
-pub fn linksrc(entry: PathBuf) -> Result<PathBuf> {
-    // value = _Path(_os.readlink(self.key.path))
-    // if not value.exists():
-    //     raise FileNotFoundError(f"{value.absolute()} is a dangling
-    // symlink")
-    //
-    // return Entry.new(value)
-    let entry = entry.read_link()?;
-    if !entry.exists() {
-        // todo
-        //   make this an error
-        panic!("{:?} is a dangling symlink", &entry)
-    }
-    Ok(entry)
 }
